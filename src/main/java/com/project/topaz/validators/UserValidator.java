f162dbdc68931d08366cc83829bb828444e7507b
mkarczewski <matkal13>
2018-04-30 14:45:01 +0200

    User details validation

---
+++ src/main/java/com/project/topaz/validators/UserValidator.java
@@ -0,0 +1,22 @@
+package com.project.topaz.validators;
+
+import com.project.topaz.web.dto.UserDto;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class UserValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return UserDto.class.isAssignableFrom(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "message.firstName", "Firstname is required.");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "message.lastName", "LastName is required.");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "message.password", "LastName is required.");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "message.username", "UserName is required.");
+    }
+}
