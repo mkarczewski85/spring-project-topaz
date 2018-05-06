f36b30d978b0aeb57baf51a05684c141bd344cc3
Maciej <m.karczewski1985@gmail.com>
2018-05-06 18:27:07 +0200

    Privilege model added

--- src/main/java/com/project/topaz/model/Privilege.java
+++ src/main/java/com/project/topaz/model/Privilege.java
@@ -1,4 +1,27 @@
 package com.project.topaz.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Data
 public class Privilege {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "privileges")
+    private List<Role> roles;
+
+    public Privilege(String name) {
+        this.name = name;
+    }
+
+
 }
