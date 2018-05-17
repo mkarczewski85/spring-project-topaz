6e93ceffaaacc2238b27c72e4fc8f9a49c92b20e
Maciej <m.karczewski1985@gmail.com>
2018-05-17 09:50:42 +0200

    Polishing

--- src/main/java/com/project/topaz/config/SecurityConfig.java
+++ src/main/java/com/project/topaz/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.project.topaz.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.session.SessionRegistry;
@@ -18,6 +21,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public SecurityConfig() {
         super();
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/h2-console").permitAll();
+        http.exceptionHandling().accessDeniedPage("/403");
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
 
     // Beans
 
--- src/main/java/com/project/topaz/config/WebConfig.java
+++ src/main/java/com/project/topaz/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig {
     @Bean
     ServletRegistrationBean h2servletRegistration() {
         ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
-        registrationBean.addUrlMappings("/console/*");
+        registrationBean.addUrlMappings("/h2-console/*");
         return registrationBean;
     }
 
